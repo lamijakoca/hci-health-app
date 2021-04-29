7b46dc9d844ce6b537d2a291bff10661b9390989
Lamija Koca <klamija99@gmail.com>
2021-04-29 23:58:15 +0200

    dt day inserted

---
+++ healthApp/backend/src/main/java/com/example/healthApp/controller/DayController.java
@@ -0,0 +1,7 @@
+package com.example.healthApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DayController {
+}
---
+++ healthApp/backend/src/main/java/com/example/healthApp/repository/DayRepository.java
@@ -0,0 +1,7 @@
+package com.example.healthApp.repository;
+
+import com.example.healthApp.model.Day;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DayRepository extends CrudRepository<Day, Long> {
+}
