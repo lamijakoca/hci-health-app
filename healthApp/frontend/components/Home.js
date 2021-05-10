3b9fbd5922134f7e85a5dca9b04ad12fcc79339f
Lamija Koca <klamija99@gmail.com>
2021-05-10 23:26:10 +0200

    home page main component added

--- healthApp/frontend/components/Home.js
+++ healthApp/frontend/components/Home.js
@@ -1,14 +1,40 @@
-import { Button, Header, Container, Content, Footer, FooterTab, Icon } from 'native-base';
+import { Button, Header, Container, Content, Footer, FooterTab, Icon, ListItem, Right, List, Left } from 'native-base';
 import * as React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import {StyleSheet, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+// import ProfilePicture from 'react-native-profile-picture';
 
 function Home({navigation}){
     return(
         <Container>
             <Header style={styles.header}>
-                <Text>Home page</Text>
+                {/* <ProfilePicture
+                isPicture={false}
+                width='35%'
+                user="Lamija Koca"
+                shape='circle'/> */}
+            <Text>
+                Username
+                {"\n"}
+                {"\n"}
+                <Text>
+                    Health Details
+                </Text>
+            </Text> 
             </Header>
-            <Content/>
+            <Content>
+                <ScrollView>
+                    <Text style={styles.history}>Medical History</Text>
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in 
+                    </Text>
+                </ScrollView>
+            </Content>
             <Footer>
                 <FooterTab>
                     <Button>
@@ -24,9 +50,22 @@ function Home({navigation}){
 }
 const styles = StyleSheet.create({
     header:{
+       height: '55%',
        top: '5%',
        backgroundColor: '#fff',
-       borderBottomColor: '#fff'
+       justifyContent: 'center',
+    //    padding: '10%',
+       display:'flex',
+    //    borderBottomColor: '#fff'
+    },
+    username: {
+        marginTop: '0%',
+        marginBottom: '10%'
+    },
+    history: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginBottom: 5
     }
 })
 export default Home;
