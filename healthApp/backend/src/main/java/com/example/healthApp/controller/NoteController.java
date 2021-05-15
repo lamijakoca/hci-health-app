164822ec6250b84baaf6aa93bf613dc2544af16a
Lamija Koca <klamija99@gmail.com>
2021-05-15 15:35:15 +0200

    crud operations for note

---
+++ healthApp/backend/src/main/java/com/example/healthApp/controller/NoteController.java
@@ -0,0 +1,44 @@
+package com.example.healthApp.controller;
+
+import com.example.healthApp.model.Message;
+import com.example.healthApp.model.Note;
+import com.example.healthApp.repository.NoteRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class NoteController {
+    NoteRepository noteRepository;
+
+    public NoteController(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @GetMapping(path = "/notes")
+    public List<Note> getAllNotes(){
+        return noteRepository.findAll();
+    }
+
+    @GetMapping(path = "note/{id}")
+    ResponseEntity<?> getNoteById(@PathVariable Long id){
+        Optional<Note> note = noteRepository.findById(id);
+        return note.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(path = "/create/note")
+    public Message createNote(@RequestBody Note note){
+            noteRepository.save(note);
+            return new Message("Successfully created!");
+    }
+
+    @DeleteMapping(path = "/delete/note/{id}")
+    public Message deleteNote(@PathVariable Long id){
+        noteRepository.deleteById(id);
+        return new Message("Successfully deleted!");
+    }
+}
