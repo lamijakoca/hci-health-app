126d248ae805813299a61038229d72178f23a661
Lamija Koca <klamija99@gmail.com>
2021-05-15 15:36:13 +0200

    note repo

---
+++ healthApp/backend/src/main/java/com/example/healthApp/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.example.healthApp.repository;
+
+import com.example.healthApp.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
