8a6ebc5fe27f24f79720bc2edf07eb6a8a7b2f01
Lamija Koca <klamija99@gmail.com>
2021-05-16 21:30:51 +0200

    sklonila welcome page iz steka i dodala Note page

--- healthApp/frontend/App.js
+++ healthApp/frontend/App.js
@@ -2,13 +2,13 @@ import * as React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import WelcomeScreen from './components/WelcomeScreen';
 import SignIn from './components/SignIn';
 import Register from './components/Register';
 import Home from './components/Home';
 import Browse from './components/Browse';
 import Activity from './components/Activity';
 import Symptoms from './components/Symptoms';
+import Note from './components/Note';
 
 const Stack = createStackNavigator();
 
@@ -16,13 +16,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
         <Stack.Screen name="SignIn" component={SignIn}/> 
         <Stack.Screen name="Register" component={Register}/>
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Browse" component={Browse}/>
         <Stack.Screen name="Activity" component={Activity}/>
         <Stack.Screen name="Symptoms" component={Symptoms}/>
+        <Stack.Screen name="Note" component={Note}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
