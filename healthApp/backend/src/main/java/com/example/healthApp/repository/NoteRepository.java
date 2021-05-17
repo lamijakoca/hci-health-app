e722b26b97127cb1fe8574867a98e004b83df5cb
Lamija Koca <klamija99@gmail.com>
2021-05-17 22:44:50 +0200

    paginacija i sortiranje +

--- healthApp/backend/src/main/java/com/example/healthApp/repository/NoteRepository.java
+++ healthApp/backend/src/main/java/com/example/healthApp/repository/NoteRepository.java
@@ -1,9 +1,13 @@
 package com.example.healthApp.repository;
 
 import com.example.healthApp.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "note", path = "note")
+public interface NoteRepository extends PagingAndSortingRepository<Note, Long> {
+    List<Note> findAllByNoteId(@Param("noteId") Long noteId);
 }
