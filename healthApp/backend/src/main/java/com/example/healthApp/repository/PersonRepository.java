511cf1e18f7b795baafb77d2e47f0d66c30eac5f
Lamija Koca <klamija99@gmail.com>
2021-05-17 22:44:36 +0200

    paginacija i sortiranje +

--- healthApp/backend/src/main/java/com/example/healthApp/repository/PersonRepository.java
+++ healthApp/backend/src/main/java/com/example/healthApp/repository/PersonRepository.java
@@ -2,9 +2,12 @@ package com.example.healthApp.repository;
 
 import com.example.healthApp.model.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
     Person findByUsername(String username);
+    List<Person> findPersonByUsername(@Param("username") String username);
 }
