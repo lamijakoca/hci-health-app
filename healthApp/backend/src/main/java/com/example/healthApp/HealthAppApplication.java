9cc2c87a0174543eabef4f969fcee5c5bbf36519
Lamija Koca <klamija99@gmail.com>
2021-05-17 22:36:18 +0200

    dodala cors mappings

--- healthApp/backend/src/main/java/com/example/healthApp/HealthAppApplication.java
+++ healthApp/backend/src/main/java/com/example/healthApp/HealthAppApplication.java
@@ -2,6 +2,9 @@ package com.example.healthApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HealthAppApplication {
@@ -10,4 +13,14 @@ public class HealthAppApplication {
 		SpringApplication.run(HealthAppApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/note**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/persons").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
