79f44d7ad2cb80109051114ab82cc5264dccc370
Lamija Koca <klamija99@gmail.com>
2021-05-18 20:46:52 +0200

    it works now

--- healthApp/frontend/components/SignIn.js
+++ healthApp/frontend/components/SignIn.js
@@ -1,102 +1,124 @@
-import axios from 'axios';
-import { Container, Content, Form, Input, Label, Item, Header, Button} from 'native-base';
-import React, {useState} from 'react';
-import { Text, StyleSheet, Image, TextInput } from 'react-native';
+import {Container,Content,Form, Input,Label,Item,Header,Button} from "native-base";
+import React, { useState } from "react";
+import { Text, StyleSheet, Image } from "react-native";
+import * as SecureToken from "expo-secure-store";
+import axios from "axios";
 
-function SignIn({navigation}){
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLogin, setIsLogin] = useState(false);
+function SignIn({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
 
-    // const loginSubmit = () => {
-    //     const User = {username, password};
-    //     axios.post('http://192.168.1.9:9009/authenticate', User)
-    //          .then((res) => {
-    //              const {token} = res.data;
-    //              localStorage.setItem('Authorization', 'Bearer ' + token);
-    //              setIsAuthorized(true);
-    //          })
-    // }
-    
-    return(
-        <Container>
-          <Header style={styles.header}>
-             {/* treba da skinem png sa sajta */}
-             <Image style={styles.logo} 
-                    source={require('../img/logo.png')}/>
-          </Header>
-            <Content style={styles.content}>
-                <Form>
-                    <Item stackedLabel>
-                        <Label> Username </Label>
-                        <Input/>
-                    </Item>
-                    <Item stackedLabel>
-                        <Label>Password</Label>
-                        <Input secureTextEntry={true}/>
-                    </Item>
-                </Form>
-                <Button rounded 
-                        style={styles.signInButton} 
-                        onPress={()=> navigation.push('Home')}>
-                    <Text style={styles.text}> 
-                        Sign in 
-                    </Text>
-                </Button>
-                <Text style={styles.question}> Don't have an account? </Text>
-                <Button style={styles.transButton} 
-                        transparent 
-                        onPress={() => navigation.push("Register")}>
-                    <Text>Register an Account</Text>
-                </Button>
-            </Content>
-        </Container>
-    )
+  const setToken = (token) => {
+    return SecureToken.setItemAsync("token", token);
+  };
+
+  const getToken = (token) => {
+    return SecureToken.getItemAsync("token");
+  };
+
+  const loginSubmit = () => {
+    const User = { username, password };
+    axios.post("http://192.168.1.11:9009/authenticate", User).then((res) => {
+      const token = res.data;
+      setToken("token", JSON.stringify(token));
+      getToken("token");
+      setIsLogin(true);
+      console.log(token);
+    });
+    if (isLogin == true) {
+      navigation.push("Home");
+    } else {
+      console.log("nije dobro ovo");
+    }
+  };
+
+  return (
+    <Container>
+      <Header style={styles.header}>
+        {/* treba da skinem png sa sajta */}
+        <Image style={styles.logo} source={require("../img/logo.png")} />
+      </Header>
+      <Content style={styles.content}>
+        <Form>
+          <Item stackedLabel>
+            <Label> Username </Label>
+            <Input value={username} onChangeText={setUsername} />
+          </Item>
+
+          <Item stackedLabel>
+            <Label>Password</Label>
+            <Input
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={true}
+            />
+          </Item>
+
+        </Form>
+
+        <Button
+          rounded
+          style={styles.signInButton}
+          onPress={() => loginSubmit()}
+        >
+          <Text style={styles.text}>Sign in</Text>
+        </Button>
+        <Text style={styles.question}> Don't have an account? </Text>
+        <Button
+          style={styles.transButton}
+          transparent
+          onPress={() => navigation.push("Register")}
+        >
+          <Text>Register an Account</Text>
+        </Button>
+      </Content>
+    </Container>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    content: {
-        padding:'10%',
-        top: '5%'
-    },
-    header: {
-        backgroundColor:'#fff',
-        borderBottomColor:'#fff',
-        height: '25%'
-    },
-    signInButton:{
-        width: 150,
-        height: 50,
-        justifyContent:'center',
-        marginTop: '10%',
-        marginLeft: '25%',
-        backgroundColor: '#ff8f9b'
-    },
-    text: {
-        color: 'white'
-    },
-    headerText:{
-        justifyContent: 'flex-start',
-        fontSize: 20,
-        color: '#67a3d9',
-        top: 40
-    },
-    question: {
-        alignSelf:'center',
-        top: '5%'
-    },
-    transButton: {
-        alignSelf: 'center',
-        top: '5%',
-        
-    },
-    logo: {
-        top: '-20%',
-    }
-}); 
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    padding: "10%",
+    top: "5%",
+  },
+  header: {
+    backgroundColor: "#fff",
+    borderBottomColor: "#fff",
+    height: "25%",
+  },
+  signInButton: {
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    marginTop: "10%",
+    marginLeft: "25%",
+    backgroundColor: "#ff8f9b",
+  },
+  text: {
+    color: "white",
+  },
+  headerText: {
+    justifyContent: "flex-start",
+    fontSize: 20,
+    color: "#67a3d9",
+    top: 40,
+  },
+  question: {
+    alignSelf: "center",
+    top: "5%",
+  },
+  transButton: {
+    alignSelf: "center",
+    top: "5%",
+  },
+  logo: {
+    top: "-20%",
+  },
+});
 
 export default SignIn;
