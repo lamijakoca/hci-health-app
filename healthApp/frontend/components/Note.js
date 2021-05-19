a330c8c261cd893c2a7b909dd8bc35aa9dc5a2db
Lamija Koca <klamija99@gmail.com>
2021-05-19 23:19:17 +0200

    izgled beleske done

--- healthApp/frontend/components/Note.js
+++ healthApp/frontend/components/Note.js
@@ -1,57 +1,66 @@
-import { Text, Container, Content, Card, CardItem, Button, Footer } from 'native-base';
-import {StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  Text,
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Button,
+  Footer,
+  Textarea,
+  Icon,
+} from "native-base";
+import { StyleSheet, TextInput } from "react-native";
+import React from "react";
 
-function Note(){
-    return(
-        <Container style={styles.container}>
-            <Content>
-                <Card style={styles.card}>
-                    <CardItem header>
-                            <Text>
-                                Ovaj header bi mogao da bude lep
-                            </Text>
-                    </CardItem>
-                    <CardItem>
-                            <Text>
-                                Plain text
-                            </Text>
-                    </CardItem>
-                    <CardItem footer>
-                        <Text>
-                            Vreme i datum neka ima
-                        </Text>
-                    </CardItem>
-                </Card>
-           </Content>
-           <Footer style={styles.footer}>
-                <Button transparent>
-                    <Text>
-                        Save
-                    </Text>
-                </Button>
-                <Button bordered>
-                    <Text>
-                        Cancel
-                    </Text>
-                </Button>
-           </Footer>
-        </Container>
-    )
+function Note({ navigation }) {
+  const date = new Date();
+
+  return (
+    <Container style={styles.container}>
+      <Content>
+        <Card style={styles.card}>
+          <CardItem header>
+            <TextInput style={styles.title} placeholder="Symptom.." />
+          </CardItem>
+          <CardItem>
+            <Textarea rowSpan={7} placeholder="How did you feel?" />
+          </CardItem>
+          <CardItem footer>
+            <Text>
+              <Icon name="calendar" /> {date.toDateString()}
+            </Text>
+          </CardItem>
+        </Card>
+      </Content>
+      <Footer style={styles.footer}>
+        <Button transparent>
+          <Text>Save</Text>
+        </Button>
+        <Button bordered onPress={navigation.push("Symptoms")}>
+          <Text>Cancel</Text>
+        </Button>
+      </Footer>
+    </Container>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignContent: 'center',
-        textAlign: 'center',
-    },
-    card: {
-        top: '10%',
-        marginLeft: '2%',
-        marginRight: '2%'
-    },
-    footer: {
-        // top:'50%'
-    }
-})
+  container: {
+    alignContent: "center",
+    textAlign: "center",
+  },
+  card: {
+    top: "10%",
+    marginLeft: "2%",
+    marginRight: "2%",
+  },
+  footer: {
+    // top:'50%'
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginLeft: "3%",
+  },
+});
 export default Note;
