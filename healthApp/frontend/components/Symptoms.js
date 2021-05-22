8660cde0164739bc889ff053dfc7f11e0fec23df
Lamija Koca <klamija99@gmail.com>
2021-05-22 17:31:51 +0200

    display notes

--- healthApp/frontend/components/Symptoms.js
+++ healthApp/frontend/components/Symptoms.js
@@ -1,9 +1,28 @@
-import { Button, Container, Header, Content } from 'native-base';
-import React from 'react'
+import { Button, Container, Header, Content, Card, CardItem } from 'native-base';
+import React, {useState, useEffect} from 'react'
 import { ScrollView } from 'react-native-gesture-handler';
 import {Text, StyleSheet, Image} from 'react-native'
+import * as SecureStore from "expo-secure-store";
+import axios from 'axios';
+import database from '../utils';
 
 function Symptoms({navigation}){
+    const [notes, setNotes] = useState([]);
+    
+    const getNotes = async() => {
+        const token = await SecureStore.getItemAsync("token");
+        if(token){
+            const res = await axios.get(`${database}/notes`, {
+                headers: {'Authorization' : `Bearer ${token}`}
+            })
+            setNotes(res.data);
+        }
+    } 
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
     return(
         <Container>
             <Header style={styles.header}>
@@ -17,7 +36,19 @@ function Symptoms({navigation}){
                         </Text>
                     </Button>
                 <ScrollView>
-                    
+                    {notes.map((note) => (
+                        <Card style={styles.card}>
+                        <CardItem header>
+                            <Text style={styles.title}>{note.title}</Text>
+                        </CardItem>
+                        <CardItem>
+                            <Text style={styles.body}>{note.body}</Text>
+                        </CardItem>
+                        <CardItem footer>
+                            <Text>Footer</Text>
+                        </CardItem>
+                    </Card>
+                    ))}
                 </ScrollView>
             </Content>
         </Container>
@@ -48,6 +79,19 @@ const styles = StyleSheet.create({
     },
     content: {
         top: '10%'
+    },
+    card: {
+        marginTop: '10%',
+        padding: '2%',
+        width: '90%',
+        alignSelf:'center'
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 16
+    },
+    body: {
+        fontSize: 16
     }
 })
 export default Symptoms;
