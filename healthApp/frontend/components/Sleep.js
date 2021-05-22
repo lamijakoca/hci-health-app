681e7b47ca5fa7c2e0b410c29e952f993b63874e
Lamija Koca <klamija99@gmail.com>
2021-05-22 20:05:39 +0200

    created component Sleep

---
+++ healthApp/frontend/components/Sleep.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {Text} from 'react-native';
+
+function Sleep(){
+    return(
+        <Text>
+            Sleep component
+        </Text>
+    )
+}
+
+export default Sleep;
