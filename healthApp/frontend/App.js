def8078db30c58a9fd5c574d6d4bacfba5c14f53
Lamija Koca <klamija99@gmail.com>
2021-05-22 20:06:40 +0200

    added screens for new components

--- healthApp/frontend/App.js
+++ healthApp/frontend/App.js
@@ -9,6 +9,9 @@ import Browse from './components/Browse';
 import Activity from './components/Activity';
 import Symptoms from './components/Symptoms';
 import Note from './components/Note';
+import Nutrition from './components/Nutrition';
+import Sleep from './components/Sleep';
+import Measurements from './components/Measurements';
 
 const Stack = createStackNavigator();
 
@@ -23,6 +26,9 @@ export default function App() {
         <Stack.Screen name="Activity" component={Activity}/>
         <Stack.Screen name="Symptoms" component={Symptoms}/>
         <Stack.Screen name="Note" component={Note}/>
+        <Stack.Screen name="Nutrition" component={Nutrition}/>
+        <Stack.Screen name="Sleep" component={Sleep}/>
+        <Stack.Screen name="Measurements" component={Measurements}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
