4c78f12c77453499e0610bd089ee952ee2914f20
Lamija Koca <klamija99@gmail.com>
2021-05-22 20:05:58 +0200

    created component Nutrition

---
+++ healthApp/frontend/components/Nutrition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function Nutrition() {
+    return(
+        <Text>
+            Nutrition component
+        </Text>
+    )
+}
+
+export default Nutrition;
