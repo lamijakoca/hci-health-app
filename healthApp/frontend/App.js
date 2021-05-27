b97e70fac73366816928b32ddda86bc0844fb789
Lamija Koca <klamija99@gmail.com>
2021-05-27 22:16:55 +0200

    clean code

--- healthApp/frontend/App.js
+++ healthApp/frontend/App.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import 'react-native-gesture-handler';
-import * as SecureStore from 'expo-secure-store';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SignIn from './components/SignIn';
+import Measurements from './components/Measurements';
+import Nutrition from './components/Nutrition';
 import Register from './components/Register';
-import Home from './components/Home';
-import Browse from './components/Browse';
 import Activity from './components/Activity';
 import Symptoms from './components/Symptoms';
-import Note from './components/Note';
-import Nutrition from './components/Nutrition';
+import Browse from './components/Browse';
+import SignIn from './components/SignIn';
 import Sleep from './components/Sleep';
-import Measurements from './components/Measurements';
+import Home from './components/Home';
+import Note from './components/Note';
+import React, {useState, useEffect} from 'react';
+import * as SecureStore from 'expo-secure-store';
+import 'react-native-gesture-handler';
 
 const Stack = createStackNavigator();
 
@@ -45,21 +45,7 @@ export default function App() {
         <Stack.Screen name="SignIn" component={SignIn}/>  
         <Stack.Screen name="Register" component={Register}/>
       </Stack.Navigator>}
-
       
-      {/* <Stack.Navigator>
-        <Stack.Screen name="SignIn" component={SignIn}/>  
-        <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Browse" component={Browse}/>
-        <Stack.Screen name="Activity" component={Activity}/>
-        <Stack.Screen name="Symptoms" component={Symptoms}/>
-        <Stack.Screen name="Note" component={Note}/>
-        <Stack.Screen name="Nutrition" component={Nutrition}/>
-        <Stack.Screen name="Sleep" component={Sleep}/>
-        <Stack.Screen name="Measurements" component={Measurements}/>
-      
-      </Stack.Navigator> */}
     </NavigationContainer>
   );
 }
